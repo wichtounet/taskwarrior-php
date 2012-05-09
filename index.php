d7f2e6a6d80de15c27623f6ed42143a7eee53237
wichtounet <baptiste.wicht@gmail.com>
2012-05-09 22:31:40 +0200

    Very first version that display pending tasks

---
+++ index.php
@@ -0,0 +1,31 @@
+<h1>My tasks</h1>
+
+<table>
+	<tr>
+		<td><strong>Project</strong></td>
+		<td><strong>Description</strong></td>
+	</tr>
+
+<?php
+	//Open the pending tasks
+	$file_handle = fopen("../taskwarrior/pending.data", "r");
+	
+	//Output all lines
+	while (!feof($file_handle)) {
+		echo "<tr>";
+
+		$line = fgets($file_handle);
+		$part = substr($line, 1, -2);
+		$parts = explode("\"", $part);
+
+		echo "<td>" . $parts[5] . "</td>";
+		echo "<td>" . $parts[1] . "</td>";
+
+		echo "</tr>";
+	}
+	
+	//Close the file
+	fclose($file_handle);
+?>
+
+</table>
