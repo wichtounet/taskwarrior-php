1670d5d00321fa5c1f338eebaeed2b942fcd9db6
wichtounet <baptiste.wicht@gmail.com>
2012-05-09 22:54:03 +0200

    Explode features into different functions

---
+++ functions.php
@@ -0,0 +1,60 @@
+<?php
+	function parse_tasks($file){
+		//Open the pending tasks
+		$file_handle = fopen($file, "r");
+		
+		//Parse all lines
+		while (!feof($file_handle)) {
+			$line = fgets($file_handle);
+			$part = substr($line, 1, -2);
+			$parts = explode("\"", $part);
+
+			$task = new Task();
+			$task->project = $parts[5];
+			$task->description = $parts[1];
+			
+			$tasks[] = $task;
+		}
+		
+		//Close the file
+		fclose($file_handle);
+		
+		return $tasks;
+	}
+	
+	function sort_tasks($tasks){
+		function cmp_task($a, $b)
+		{
+			return strcmp($a->project, $b->project);
+		}
+
+		usort($tasks, "cmp_task");
+	}
+	
+	function display_as_table($tasks){
+		echo table_header();
+		
+		//Output all tasks
+		foreach($tasks as $task){
+			echo "<tr>";
+			echo "<td>" . $task->project . "</td>";
+			echo "<td>" . $task->description . "</td>";
+			echo "</tr>";
+		}
+		
+		echo table_footer();
+	}
+	
+	function table_header(){
+		return "<h1>My tasks</h1>". 
+			"<table>
+				<tr>
+					<td><strong>Project</strong></td>
+					<td><strong>Description</strong></td>
+				</tr>";
+	}
+	
+	function table_footer(){
+		return "</table>";
+	}
+?>
--- index.php
+++ index.php
@@ -1,50 +1,11 @@
-<h1>My tasks</h1>
-
-<table>
-	<tr>
-		<td><strong>Project</strong></td>
-		<td><strong>Description</strong></td>
-	</tr>
-
 <?php
 	include "config.php";
 	include "Task.php";
+	include "functions.php";
 
-	//Open the pending tasks
-	$file_handle = fopen($PENDING_DATA_PATH, "r");
+	$tasks = parse_tasks($PENDING_DATA_PATH);
 	
-	//Parse all lines
-	while (!feof($file_handle)) {
-		$line = fgets($file_handle);
-		$part = substr($line, 1, -2);
-		$parts = explode("\"", $part);
-
-		$task = new Task();
-		$task->project = $parts[5];
-		$task->description = $parts[1];
-		
-		$tasks[] = $task;
-	}
+	sort_tasks($tasks);
 	
-	//Close the file
-	fclose($file_handle);
-	
-	function cmp_task($a, $b)
-	{
-		return strcmp($a->project, $b->project);
-	}
-
-	usort($tasks, "cmp_task");
-	
-	//Output all tasks
-	foreach($tasks as $task){
-		echo "<tr>";
-		
-		echo "<td>" . $task->project . "</td>";
-		echo "<td>" . $task->description . "</td>";
-
-		echo "</tr>";
-	}
+	display_as_table($tasks);
 ?>
-
-</table>
