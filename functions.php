6d7d3bf555d50236389d6eadc9c42e737acde14c
wichtounet <baptiste.wicht@gmail.com>
2012-05-11 20:19:06 +0200

    Add function to generate a new uuid()

--- functions.php
+++ functions.php
@@ -133,4 +133,16 @@
 
         return round($completed_cnt / ($pending_cnt + $completed_cnt), 2);
     }
+	
+	function GUID(){
+		if (function_exists('com_create_guid') === true){
+			return trim(com_create_guid(), '{}');
+		}
+
+		return sprintf('%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(16384, 20479), mt_rand(32768, 49151), mt_rand(0, 65535), mt_rand(0, 65535), mt_rand(0, 65535));
+	}
+	
+	function uuid(){
+		return strtolower(GUID());
+	}
 ?>
