88ecd5193315b54d45b484a2fc18f2d46cf43188
wichtounet <baptiste.wicht@gmail.com>
2012-05-11 21:13:46 +0200

    Add a very simple form to create tasks

---
+++ insert.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+		<h1>Insert a new task</h1>
+	
+		<form action="actions.php" method="get">
+			<input type='hidden' id="action" value="insert" />
+			Description: <input type="text" id="description" /> <br />
+			Project: <input type="text" id="project" /> <br />
+			<input type="submit" />
+		</form>
+	</body>
+</html>
