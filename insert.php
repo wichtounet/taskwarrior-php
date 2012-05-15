ce40935d608a8c6c7068ebb6f1ff03bdd6e783b7
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-05-15 14:35:54 +0200

    Change the order of fields in the insert form

--- insert.php
+++ insert.php
@@ -4,8 +4,8 @@
 	
 		<form action="index.php" method="get">
 			<input type='hidden' name="action" value="insert" />
-			Description: <input type="text" name="description" /> <br />
 			Project: <input type="text" name="project" /> <br />
+			Description: <input type="text" name="description" /> <br />
 			<input type="submit" />
 		</form>
 	</body>
